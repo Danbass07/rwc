6fd1521a45d419aafb1e9a91b4d5c3d1384ee012
Danbass07 <danbass666@googlemail.com>
2018-09-26 14:33:24 +0100

    first commit

---
+++ resources/js/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Body from './components/Body';
+
+
+
+if (document.getElementById('root')) {
+    ReactDOM.render(
+
+    <Body />
+    
+    
+    , document.getElementById('root'));
+}
